e52f0a6f3e54971a81c964dcf79f6ca82c0d5fa7
lpnguyen <Nguyen.la@axonactive.com>
2020-09-19 12:26:34 +0700

    sdfsdf

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/about', function (req, res) {
-  res.send('Hello world');
+  res.send('Hello worlds sf sdf sdfsdfs');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
