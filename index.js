297cdc93dffdc4b990f98dede094ad3cfb28a9f5
lpnguyen <Nguyen.la@axonactive.com>
2020-09-19 16:03:22 +0700

    change file index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/about', function (req, res) {
-  res.send('gio thu in lai message moi coi con dung khong nha');
+  res.send('gio thu in lai messag');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
